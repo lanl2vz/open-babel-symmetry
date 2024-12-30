841d7b86f25a182d210f6e6e04f26a7d7e70ded6
lanl2tz <lanl2tz@hotmail.com>
2024-12-31 00:15:47 +0800

    move molecule

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <openbabel/obconversion.h>
 #include <openbabel/mol.h>
 #include <fstream>
+#include <iostream>
 
 using namespace OpenBabel;
 
@@ -9,9 +10,27 @@ int main()
   OBConversion conv;
   conv.SetInFormat("sdf");
 
-  std::ifstream ifs("../resource/molecule.sdf");
+  std::ifstream ifs("molecule.sdf");
+  if (!ifs)
+  {
+    std::cerr << "Error: Failed to open molecule.sdf" << std::endl;
+    if (std::ifstream("molecule.sdf").fail())
+    {
+      std::cerr << "File does not exist or is inaccessible." << std::endl;
+    }
+    else
+    {
+      std::cerr << "Unknown error occurred while opening the file." << std::endl;
+    }
+    return 1;
+  }
+
   OBMol obmol;
-  conv.Read(&obmol, &ifs);
+  if (!conv.Read(&obmol, &ifs))
+  {
+    std::cerr << "Error: Failed to read molecule.sdf" << std::endl;
+    return 1;
+  }
 
   return 0;
 }
